16629d052876afaef9023b929b53f2518d63eadc
eborror <eborror@oakland.edu>
2025-03-26 12:41:04 -0400

    Create SoftwareDeveloper.java

---
+++ company/roles/SoftwareDeveloper.java
@@ -0,0 +1,9 @@
+package company.roles;
+
+// Class: Software Engineer implementing Role
+public class SoftwareDeveloper implements Role {
+    @Override
+    public void performDuties() {
+        System.out.println("Software Developer is developing applications.");
+    }
+}
